520452f6afb83680b5464187395876ea22426e1e
jpilimon <jakub.pilimon@gmail.com>
2022-01-16 13:18:53 +0100

    rename

--- src/test/java/io/pillopl/dietary/TaxConfigControllerTest.java
+++ src/test/java/io/pillopl/dietary/TaxConfigControllerTest.java
@@ -12,7 +12,7 @@ import static io.pillopl.dietary.TaxRule.squareRule;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class TaxRuleControllerTest {
+class TaxConfigControllerTest {
 
     String countryCode = "country-code";
     String countryCode2 = "country-code2";
